c33919b5428c652958d855227032765819fb52a8
Marius Damian <marius.damian.stefan@gmail.com>
2022-03-08 14:10:46 +0200

    1

--- pages/components/Navbar/SearchBar.js
+++ pages/components/Navbar/SearchBar.js
@@ -11,7 +11,7 @@ function SearchBar() {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
     const newFilter = products.filter((value) => {
-      return value.name.toLowerCase().includes(searchWord.toLowerCase());
+      return value.name.toLowerCase().includes(searchWord.toLowerCase()) || value.fullDescription.toLowerCase().includes(searchWord.toLowerCase()) || value.smallDescription.toLowerCase().includes(searchWord.toLowerCase());
     });
     if (searchWord === "") {
       setFilteredData([]);
@@ -27,17 +27,21 @@ function SearchBar() {
   return (
     <div className=' h-full'>
       <div className='relative'>
-        <input type='text' className='rounded-2xl h-8 bg-gray-200 px-5 text-black font-normal w-60 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:border-transparent shadow-2xl' placeholder='Caută produs...' onChange={handleFilter} value={wordEntered} />
+        <input type='text' className={`rounded-2xl h-8 bg-gray-200 px-5 text-black font-normal w-44 focus:w-96 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:border-transparent shadow-2xl ${wordEntered.length != 0 ? "w-96" : ""}`} placeholder='Caută produs...' onChange={handleFilter} value={wordEntered} />
         <div className='absolute inset-y-0 right-0 text-2xl mr-2 mt-1 text-blue-600'>{wordEntered.length === 0 ? <FcSearch /> : <AiFillCloseCircle onClick={clearInput} className='cursor-pointer' />}</div>
       </div>
       {filteredData.length != 0 && (
-        <div className='overflow-hidden h-56 overflow-y-auto absolute w-60 scrollbar-hidden bg-gray-200 p-2 rounded-2xl text-black mt-2'>
+        <div className='overflow-hidden h-96 overflow-y-auto absolute w-96 scrollbar-hidden bg-gray-200 p-2 rounded-l-2xl text-black mt-2'>
           {filteredData.map((produs, index) => (
             <Link key={index} href={`/${produs.category}/${produs.subcategory}/${produs.name}`.replaceAll(" ", "-").toLocaleLowerCase()}>
               <a href='' onClick={clearInput}>
                 <div className='flex flex-row items-center mb-3'>
-                  <img src={`/images/productImage/thumbnail/${produs.Thumbnail}`} alt='' className='w-20 mr-3 rounded-xl bg-[#00001a]' />
-                  <p>{produs.name}</p>
+                  <img src={`/images/productImage/thumbnail/${produs.Thumbnail}`} alt='' className='w-20 p-1 mr-3 rounded-xl bg-[#00001a]' />
+                  <div className='flex flex-col hover:text-blue-900'>
+                    <p>{produs.name}</p>
+                    <p className='text-xs text-gray-800 hover:text-blue-900'>{produs.smallDescription}</p>
+                    <p className='text-[10px] mt-1'>{`${produs.category} > ${produs.subcategory}`}</p>
+                  </div>
                 </div>
               </a>
             </Link>
