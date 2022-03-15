47170b1e121efb794877d6d95fc87dc2e87a8832
Marius Damian <marius.damian.stefan@gmail.com>
2022-03-15 11:20:06 +0200

    1

--- pages/components/HomePage/AboutSection.js
+++ pages/components/HomePage/AboutSection.js
@@ -3,33 +3,34 @@ import Typed from "react-typed";
 
 function AboutSection() {
   return (
-    <div className='min-h-screen w-full flex flex-row text-gray-200 leading-8' id='despre'>
-      <div className='w-1/2'>
-        <img src='./images/about3.webp' alt='' className='' />
+    <div className='min-h-screen w-full flex flex-col items-center justify-start text-gray-200 leading-8 max-w-7xl mx-auto pb-20' id='despre'>
+      <div className='py-24 flex flex-col items-center justify-center text-center space-y-3'>
+        <h1 className='lg:text-5xl text-2xl'>Alpha Medical</h1>
+        <h2 className='lg:text-2xl text-2xl'>
+          <Typed strings={["Distribuitori Exclusivi.", "Service Autorizat.", "Echipamente Medicale.", "Certificare ISO 9001 si ISO 13485"]} typeSpeed={120} backSpeed={15} backDelay={1000} loop />
+        </h2>
       </div>
-      <div className='w-1/2 min-h-screen flex items-center'>
-        <div className='bg-[#00001a] w-full min-h-[80%] -ml-52 rounded-3xl flex flex-col py-28 pl-24 space-y-8'>
-          <h1 className='lg:text-4xl text-2xl font-medium pb-6'>
-            <Typed strings={["Distribuitori Exclusivi.", "Service Autorizat.", "Echipamente Medicale.", "Certificare ISO 9001 si ISO 13485"]} typeSpeed={120} backSpeed={15} backDelay={1000} loop />
-          </h1>
-          <div className='flex flex-row items-center space-x-10 mt-20'>
-            <img src='./images/international.png' alt='' className='' />
-            <p>Avem o abordare holistica asupra specialitatilor medicale, asigurand solutii complete prin colaborarea cu numeroase companii de prestigiu din Europa, Statele Unite si Asia, pentru a oferi sistemului medical romanesc o gama variata de produse, la cele mai inalte standarde.</p>
-          </div>
-          <div className='flex flex-row items-center space-x-10'>
-            <img src='./images/serv.png' alt='' className='' />
-            <p>Dispunem de un service complet autorizat de catre Ministerul Sanatatii, cu tehnicieni si inginerii instruiti periodic si cu o vasta experienta in domeniul echipamentelor medicale.</p>
-          </div>
-          <div className='flex flex-row items-center space-x-10'>
-            <img src='./images/award.png' alt='' className='' />
-            <p>Detinem certiﬁcare ISO 9001 si ISO 13485, demonstrand abilitatea de a mentine constant standarde ridicate in activitatile de import, distributie, depozitare si mentenanta dispozitive medicale.</p>
-          </div>
-          <div className='flex flex-row items-center space-x-10'>
-            <img src='./images/distr.png' alt='' className='' />
-            <p>Suntem distribuitori exclusivi ai urmatorilor producatori: 3M (selectie de produse), Steris Corporation (Sterilizare), Teleflex Medical, Hamilton Medical, Fujifilm SonoSite etc.</p>
-          </div>
+      <div className='flex flex-row space-x-5 py-10'>
+        <div className='transform transition-all duration-700 hover:scale-105 h-[600px]'>
+          <img src='./images/1.png' alt='' className="mt-2"/>
+        </div>
+        <div className='transform transition-all duration-700 hover:scale-105 h-[600px]'>
+          <img src='./images/2.png' alt='' className="mt-6"/>
+        </div>
+        <div className='transform transition-all duration-700 hover:scale-105 h-[600px]'>
+          <img src='./images/3.png' alt='' className="mt-2"/>
+        </div>
+        <div className='transform transition-all duration-700 hover:scale-105 h-[600px]'>
+          <img src='./images/4.png' alt='' className="mt-6"/>
+        </div>
+        <div className='transform transition-all duration-700 hover:scale-105 h-[600px]'>
+          <img src='./images/5.png' alt='' className="mt-2"/>
         </div>
       </div>
+      <h1 className='my-3 text-left w-full text-3xl'>Lorem ipsum dolor sit </h1>
+      <h2 className='text-base font-light w-full text-left'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br></br> Alias et repellat blanditiis beatae quod quia eligendi, veritatis, necessitatibus itaque ut ex nulla, aliquam illum. <br></br> Magnam modi ut maxime officiis laboriosam!
+      </h2>
     </div>
   );
 }
