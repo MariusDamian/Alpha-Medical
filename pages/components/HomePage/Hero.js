d3d3ae21fcc5ebc9cf77ed2b7634eb94a621439a
Marius Damian <marius.damian.stefan@gmail.com>
2022-04-30 20:56:57 +0300

    Add carousel buttons for navigation

--- pages/components/HomePage/Hero.js
+++ pages/components/HomePage/Hero.js
@@ -5,17 +5,32 @@ import carousel from "../JSON/carousel.json";
 
 function Hero() {
   const [crsNumber, setCrsNumber] = useState(0);
+
   useEffect(() => {
-    setTimeout(function () {
-      crsNumber < carousel.length - 1 ? setCrsNumber(crsNumber + 1) : setCrsNumber(0);
+    const intervalId = setInterval(() => {
+      if (crsNumber === carousel.length - 1) {
+        setCrsNumber(0);
+      } else {
+        setCrsNumber(crsNumber + 1);
+      }
     }, 6000);
+
+    return () => clearInterval(intervalId);
   }, [crsNumber]);
 
   return (
     <div className='bg-alpha-bg h-screen'>
       <div className='h-full pt-36 flex flex-row'>
-        <div className='w-1/2 flex flex-col items-center justify-center h-full'>
-          <div className='flex flex-col items-start justify-center'>
+        <div className='w-1/2 flex flex-col items-center justify-center h-full relative'>
+          <div className='space-x-2 absolute right-0 top-0 p-2'>
+            <button onClick={() => (crsNumber === 0 ? setCrsNumber(carousel.length - 1) : setCrsNumber(crsNumber - 1))}>
+              <img src='./images/left-arrow.svg' alt='' />
+            </button>
+            <button onClick={() => (crsNumber < carousel.length - 1 ? setCrsNumber(crsNumber + 1) : setCrsNumber(0))}>
+              <img src='./images/right-arrow.svg' alt='' />
+            </button>
+          </div>
+          <div className='flex flex-col items-start justify-center w-full max-w-2xl'>
             <AlphaTitle title={carousel[crsNumber].crsTitle} />
             <div className='max-w-xs my-11'>
               <AlphaSubtitle title={carousel[crsNumber].crsDesc} />
@@ -23,7 +38,7 @@ function Hero() {
             <button className='css-button-sliding-to-left--green'>DESCOPERA</button>
           </div>
         </div>
-        <div className={`w-1/2 bg-[url("/images/${carousel[crsNumber].crsImg}")] bg-cover`}></div>
+        <img src={`/images/crsImage/${carousel[crsNumber].crsImg}`} className='w-1/2' alt='' />
       </div>
     </div>
   );
