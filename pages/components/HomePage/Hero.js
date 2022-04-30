d34910377a5adfd8a3325ea85b9d06195a34e16d
Marius Damian <marius.damian.stefan@gmail.com>
2022-04-30 20:23:11 +0300

    Menu slide

--- pages/components/HomePage/Hero.js
+++ pages/components/HomePage/Hero.js
@@ -1,50 +1,21 @@
 import React from "react";
 import AlphaTitle from "../Reutils/AlphaTitle";
 import AlphaSubtitle from "../Reutils/AlphaSubtitle";
-import { useEffect, useState } from "react/cjs/react.development";
-import carousel from "../JSON/carousel.json";
-import { Fade } from "react-reveal";
 
 function Hero() {
-  const [crsNumber, setCrsNumber] = useState(0);
-
-  useEffect(() => {
-    setTimeout(function () {
-      crsNumber < carousel.length - 1 ? setCrsNumber(crsNumber + 1) : setCrsNumber(0);
-      console.log(crsNumber);
-    }, 6000);
-  }, [crsNumber]);
-
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     if (crsNumber === carousel.length - 1) {
-  //       setCrsNumber(0);
-  //     } else {
-  //       setCrsNumber(crsNumber + 1);
-  //     }
-  //   }, 5000);
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
   return (
     <div className='bg-alpha-bg h-screen'>
       <div className='h-full pt-36 flex flex-row'>
         <div className='w-1/2 flex flex-col items-center justify-center h-full'>
-          <div className='flex flex-col items-start justify-center max-w-[600px] w-full'>
-            <div className='w-full'>
-              <AlphaTitle title={carousel[crsNumber]?.crsTitle} />
-            </div>
+          <div className='flex flex-col items-start justify-center'>
+            <AlphaTitle title='HAMILTON-C6' />
             <div className='max-w-xs my-11'>
-              <AlphaSubtitle title={carousel[crsNumber]?.crsDesc} />
+              <AlphaSubtitle title='Generatia noua de ventilatoare performante' />
             </div>
             <button className='css-button-sliding-to-left--green'>DESCOPERA</button>
           </div>
         </div>
-        <Fade>
-          {/* <div className='w-1/2 bg-[url("/images/crsImage/carusel-3.webp")] bg-cover transition-all duration-700'></div> */}
-          <div className={`w-1/2 bg-[url("/images/crsImage/${carousel[crsNumber]?.crsImg}")] bg-cover transition-all duration-700`}></div>
-        </Fade>
+        <div className='w-1/2 bg-[url("/images/home-1.png")] bg-cover'></div>
       </div>
     </div>
   );
--- pages/components/Navbar/Menu.js
+++ pages/components/Navbar/Menu.js
@@ -7,33 +7,33 @@ function Menu() {
   const { menu, setMenu } = useContext(dataContext);
   return (
     <Slide direction='right' in={menu} style={{ zIndex: 10 }} transition={{ exit: { duration: 1 } }}>
-        <div className='w-[550px] bg-black/80 h-full fixed top-0 right-0 z-20 flex flex-col items-start justify-center space-y-5 text-white px-10 backdrop-blur-xl'>
-          <div className='text-2xl space-y-4 flex flex-col items-start mt-10'>
-            <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
-              <BsPlus className='inline-block mb-1 text-3xl' />
-              Acasa
-            </button>
-            <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
-              <BsPlus className='inline-block mb-1 text-3xl' />
-              Produse
-            </button>
-            <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
-              <BsPlus className='inline-block mb-1 text-3xl' />
-              Parteneri
-            </button>
-            <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
-              <BsPlus className='inline-block mb-1 text-3xl' />
-              Contact
-            </button>
-          </div>
-          <div>Dark mode</div>
-          <div className='max-w-[300px]'>
-            <h1 className='text-2xl'>Adresa</h1>
-            <p className='my-3'>Adresa Str. Sandu-Aldea Constantin nr.22, Sector 1, Bucureşti Romania</p>
-            <p>Phone: +40 21 323 14 23 </p>
-            <p>Mail: office@alphamedical.ro</p>
-          </div>
+      <div className='w-[550px] bg-black/80 h-full fixed top-0 right-0 z-20 flex flex-col items-start justify-center space-y-5 text-white px-10 backdrop-blur-xl'>
+        <div className='text-2xl space-y-4 flex flex-col items-start mt-10'>
+          <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
+            <BsPlus className='inline-block mb-1 text-3xl' />
+            Acasa
+          </button>
+          <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
+            <BsPlus className='inline-block mb-1 text-3xl' />
+            Produse
+          </button>
+          <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
+            <BsPlus className='inline-block mb-1 text-3xl' />
+            Parteneri
+          </button>
+          <button className='hover:text-alpha-green font-semibold transition-all duration-300'>
+            <BsPlus className='inline-block mb-1 text-3xl' />
+            Contact
+          </button>
         </div>
+        <div>Dark mode</div>
+        <div className='max-w-[300px]'>
+          <h1 className='text-2xl'>Adresa</h1>
+          <p className='my-3'>Adresa Str. Sandu-Aldea Constantin nr.22, Sector 1, Bucureşti Romania</p>
+          <p>Phone: +40 21 323 14 23 </p>
+          <p>Mail: office@alphamedical.ro</p>
+        </div>
+      </div>
     </Slide>
   );
 }
