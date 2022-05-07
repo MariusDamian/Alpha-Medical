1a608ad53444ffaa6db89e78d4d92b455194713a
Marius Damian <marius.damian.stefan@gmail.com>
2022-05-07 13:01:03 +0300

    Update

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Menu from "./components/Navbar/Menu";
 import Navbar from "./components/Navbar/Navbar";
-import { gMaps } from "./components/Reutils/Consts";
 import { MdLocationPin } from "react-icons/md";
 import { BsEnvelope, BsTelephone } from "react-icons/bs";
 
@@ -31,7 +30,17 @@ function Contact() {
                 <button className='bg-alpha-green w-full h-10 text-white'>Trimite</button>
               </form>
             </div>
-            <div className='w-[1100px]'>{gMaps}</div>
+            <div className='w-[1100px]'>
+              {" "}
+              <iframe
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.304637529315!2d26.062718515555794!3d44.46792800738279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b20223548f5fa9%3A0xd06b47e956eba9e7!2sStrada%20Constantin%20Sandu-Aldea%2022%2C%20Bucure%C8%99ti%20012244!5e0!3m2!1sen!2sro!4v1651868451083!5m2!1sen!2sro'
+                width='1100'
+                height='450'
+                allowFullScreen=''
+                loading='lazy'
+                referrerpolicy='no-referrer-when-downgrade'
+              ></iframe>
+            </div>
           </div>
           <div className='flex flex-row h-[300px] mt-2 space-x-2'>
             <div className='w-[340px] bg-[#E3E3E3] flex flex-col items-start justify-start pt-24 px-4 text-left pl-14 relative'>
