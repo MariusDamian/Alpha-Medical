0ee3ee97cb3178e6f958a5521512f45d5511b26c
Marius Damian <marius.damian.stefan@gmail.com>
2022-05-08 23:05:26 +0300

    Fix dark btn

--- pages/components/Navbar/Menu.js
+++ pages/components/Navbar/Menu.js
@@ -41,9 +41,9 @@ function Menu() {
           </Link>
         </div>
         <div className='border-t-2 border-b-2 border-alpha-green'>
-          <div onClick={toggleDark} className='py-4 w-32'>
-            {dark ? <img src='./images/darkbtn.svg' alt='' /> : <img src='./images/lightbtn.svg' alt='' />}
-          </div>
+          <button onClick={toggleDark} className='py-4 w-32'>
+            {dark ? <img src='../images/darkbtn.svg' alt='' /> : <img src='../images/lightbtn.svg' alt='' />}
+          </button>
         </div>
         <div className='max-w-[300px]'>
           <h1 className='text-2xl'>Adresa</h1>
